dc40f5e8827177d28018811a7c5b4fee47283997
Logan-Myn <logan.moyon15@gmail.com>
2025-01-25 14:13:29 +0200

    About_HeroDefaultButton

--- components/PageBuilder.tsx
+++ components/PageBuilder.tsx
@@ -69,6 +69,9 @@ export default function PageBuilder({
       content: {
         title: 'Add a title',
         subtitle: 'Add a subtitle',
+        ...(selectedSectionType === 'hero' && {
+          buttonType: 'join'
+        })
       },
       order: sections.length,
     };
